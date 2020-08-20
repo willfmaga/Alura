d058f8bd1383f374da3688603d5c0789a56e24e1
William F Magalhães <william.magalhaes@edenred.com>
2020-08-20 16:14:01 -0300

    EF e tag helper

--- CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CasaDoCodigo.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +11,16 @@ namespace CasaDoCodigo.Controllers
 {
     public class PedidoController : Controller
     {
-      
+        private readonly IProdutoRepository produtoRepository;
+
+        public PedidoController(IProdutoRepository produtoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+        }
+
         public IActionResult Carrossel()
         {
-            return View();
+            return View(produtoRepository.GetProdutos());
         }
 
         public IActionResult Carrinho()
--- CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CasaDoCodigo.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,6 +24,18 @@ namespace CasaDoCodigo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            string connectionString = Configuration.GetConnectionString("Default");
+
+            services.AddDbContext<ApplicationContext>(options => 
+                options.UseSqlServer(connectionString)
+            );
+
+            services.AddTransient<IDataService, DataService>();
+            services.AddTransient<IProdutoRepository,ProdutoRepository>();
+            services.AddTransient<IPedidoRepository,PedidoRepository>();
+            services.AddTransient<IItemPedidoRepository,ItemPedidoRepository>();
+            services.AddTransient<ICadastroRepository, CadastroRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,8 +57,11 @@ namespace CasaDoCodigo
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
+                    template: "{controller=Pedido}/{action=Carrossel}/{codigo?}");
             });
+
+            serviceProvider.GetService<IDataService>()
+                .InicializaDB();
         }
     }
 }
