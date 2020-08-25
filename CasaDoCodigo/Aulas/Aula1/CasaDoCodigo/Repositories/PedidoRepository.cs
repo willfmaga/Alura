9be4003f9c8403ae20580b638f670bf7d0d264c1
William F Magalhães <william.magalhaes@edenred.com>
2020-08-25 16:52:25 -0300

    Finalizando Curso

--- CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -65,7 +65,8 @@ namespace CasaDoCodigo.Repositories
 
             var pedido = dbSet
                     .Include(p => p.Itens)
-                    .ThenInclude(i => i.Produto)
+                        .ThenInclude(i => i.Produto)
+                    .Include(p => p.Cadastro)
                     .Where(p => p.Id == pedidoId)
                     .SingleOrDefault();
 
